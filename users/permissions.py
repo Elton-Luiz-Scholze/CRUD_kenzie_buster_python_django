03bed96f5be812abcb7f0decf926d3d3e3a5c8ce
Elton Luiz Scholze <elscholze@gmail.com>
2023-02-17 15:28:50 -0300

    feat: created GET route list a specific user

---
+++ users/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+from rest_framework.views import Request, View
+from .models import User
+
+
+class IsUserPermission(permissions.BasePermission):
+    def has_object_permission(self, req: Request, view: View, obj: User):
+        return req.user == obj or req.user.is_employee
--- users/views.py
+++ users/views.py
@@ -1,5 +1,11 @@
 from rest_framework.views import APIView, Request, Response, status
 from .serializers import UserSerializer
+from .models import User
+from django.shortcuts import get_object_or_404
+from movies.permissions import IsEmployeePermission
+from .permissions import IsUserPermission
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserView(APIView):
@@ -11,3 +17,17 @@ class UserView(APIView):
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class SpecificUserView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployeePermission, IsUserPermission, IsAuthenticated]
+
+    def get(self, req: Request, user_id):
+        user = get_object_or_404(User, pk=user_id)
+
+        self.check_object_permissions(req, user)
+
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
